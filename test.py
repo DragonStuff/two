b2eaa728a3d127e7afba839b69701da68d9a3b02
Alexander <4584443+DragonStuff@users.noreply.github.com>
2020-11-19 11:26:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 from multiprocessing import Pool
-import sys, requests
+import sys, requests, time
 
 def request(line):
+	time.sleep(0.5)
 	user = line.strip()
 	status = requests.get(f"https://github.com/{user}").status_code
 	result = {"user": user, "data": status}
 	if status == 404: print(user)
 	return result
 
-pool = Pool(processes=4)
+pool = Pool(processes=1)
 
 try:
 	lines = sys.stdin.readlines()
